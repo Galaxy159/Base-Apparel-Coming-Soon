8803fe2731002fc21c9fb4daa81e3c147f15b62b
Galaxy159 <braun.asaf@gmail.com>
2022-10-29 00:31:12 +0300

    save

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use-strict";
+
+const input = document.querySelector(".component__email--input");
+const errorSign = document.querySelector(".component__email--error-sign");
+const errorMsg = document.querySelector(".component__email--error-msg");
+const inputBtn = document.querySelector(".component__email--btn");
+
+const validateEmail = function (email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+};
+
+inputBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!validateEmail(input.value)) {
+    errorMsg.classList.remove("hide");
+    errorSign.classList.remove("hide");
+  } else {
+    errorMsg.classList.add("hide");
+    errorSign.classList.add("hide");
+  }
+});
